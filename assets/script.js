c66d6082b365a7b18ff1b5473fde742d6e65b3da
adamarispadilla <72954890+adamarispadilla@users.noreply.github.com>
2023-05-18 15:42:00 +0900

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -7,14 +7,6 @@ $(document).ready(function(){
           $(this).toggleClass('active');
      });
 
-     //---------------------------
-     // setup show hide all button
-     //---------------------------
-     $("#showhide").click(function(){
-          
-          $('.item').toggleClass('active');
-     });
-
      //------------------------------------
      // adds a a darkmode class to the body
      //------------------------------------
