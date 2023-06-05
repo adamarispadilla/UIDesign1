83704a12c3355a4d996cb37637210d7c68666acc
adamarispadilla <72954890+adamarispadilla@users.noreply.github.com>
2023-06-06 03:15:39 +0900

    Update script.js

--- P2/assets/script.js
+++ P2/assets/script.js
@@ -1,7 +1,7 @@
 
 
 $(document).ready(function() {
-  $('css/relax/rcloud1.png').click(function(e) {
+  $('cloud1.png').click(function(e) {
     e.preventDefault();
     var imageUrl = $(this).attr('src');
     $('#overlay-image').attr('src', imageUrl);
