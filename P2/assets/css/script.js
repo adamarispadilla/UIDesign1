ea66b58bdd63ce93dba11a681da76d6ea5907df9
adamarispadilla <72954890+adamarispadilla@users.noreply.github.com>
2023-06-06 03:18:45 +0900

    scrip

--- P2/assets/css/script.js
+++ P2/assets/css/script.js
@@ -1,7 +1,7 @@
 
 
 $(document).ready(function() {
-  $('rcloud1.png').click(function(e) {
+  $('relax/rcloud1.png').click(function(e) {
     e.preventDefault();
     var imageUrl = $(this).attr('src');
     $('#overlay-image').attr('src', imageUrl);
@@ -12,3 +12,4 @@ $(document).ready(function() {
     $(this).fadeOut();
   });
 });
+
