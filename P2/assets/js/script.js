1afadc5124e4a41f0207cf2008ac927976871fbc
adamarispadilla <72954890+adamarispadilla@users.noreply.github.com>
2023-06-07 03:07:57 +0900

    Update script.js

--- P2/assets/js/script.js
+++ P2/assets/js/script.js
@@ -2,11 +2,15 @@ $(document).ready(function() {
   $(".rcloud1, .rcloud2, .rcloud3, .rcloud4, .rcloud5, .rcloud6, .rcloud7, .rcloud8, .rcloud9").click(function() {
     var $this = $(this);
     var imageSrc = $this.attr("src");
+    var text = $this.siblings(".rcloud-text").text();
+
     var text = $(this).siblings(".rcloud-text").text();
     var overlayText = $this.closest(".cloud-container2").data("text");
 
     $("#overlay-image").attr("src", imageSrc);
     $("#overlay-message").text(text);
+    $("#overlay-message").text(overlayText);
+
     $("#overlay-link").text(text);
 
     $("#overlay").fadeIn();
