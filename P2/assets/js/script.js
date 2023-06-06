76bc32c1c13c024ba9457f7a095a15640c024a36
adamarispadilla <72954890+adamarispadilla@users.noreply.github.com>
2023-06-07 02:07:09 +0900

    Update script.js

--- P2/assets/js/script.js
+++ P2/assets/js/script.js
@@ -1,16 +1,4 @@
 $(document).ready(function() {
-  $(".rcloud1, .rcloud2, .rcloud3,.rcloud4, .rcloud5, .rcloud6, .rcloud7, .rcloud8, .rcloud9").click(function() {
-    var imageSrc = $(this).attr("src");
-    $("#overlay-image").attr("src", imageSrc);
-    $("#overlay").fadeIn();
-  });
-
-  $("#overlay").click(function() {
-    $("#overlay").fadeOut();
-  });
-});
-
-$(document).ready(function() {
   $(".rcloud1, .rcloud2, .rcloud3, .rcloud4, .rcloud5, .rcloud6, .rcloud7, .rcloud8, .rcloud9").click(function() {
     var $this = $(this);
     var imageSrc = $this.attr("src");
