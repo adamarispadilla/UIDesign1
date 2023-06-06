c947aa12d5122a3ad50f516b6a642c58b608d5a0
adamarispadilla <72954890+adamarispadilla@users.noreply.github.com>
2023-06-07 02:02:31 +0900

    change

--- P2/assets/js/script.js
+++ P2/assets/js/script.js
@@ -10,3 +10,20 @@ $(document).ready(function() {
   });
 });
 
+$(document).ready(function() {
+  $(".rcloud1, .rcloud2, .rcloud3, .rcloud4, .rcloud5, .rcloud6, .rcloud7, .rcloud8, .rcloud9").click(function() {
+    var $this = $(this);
+    var imageSrc = $this.attr("src");
+    var text = $this.next(".overlay-text");
+
+    $("#overlay-image").attr("src", imageSrc);
+    text.css("opacity", "1");
+
+    $("#overlay").fadeIn();
+  });
+
+  $("#overlay").click(function() {
+    $(".overlay-text").css("opacity", "0");
+    $("#overlay").fadeOut();
+  });
+});
