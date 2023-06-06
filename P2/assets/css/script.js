1b8c8abf923f38623d904ba0325249c42f795e8b
adamarispadilla <72954890+adamarispadilla@users.noreply.github.com>
2023-06-07 00:03:35 +0900

    rclouds overlay

--- P2/assets/css/script.js
+++ P2/assets/css/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $(".rcloud1, .rcloud2, .rcloud3").click(function() {
+  $(".rcloud1, .rcloud2, .rcloud3,.rcloud4, .rcloud5, .rcloud6, .rcloud7, .rcloud8, .rcloud9").click(function() {
     var imageSrc = $(this).attr("src");
     $("#overlay-image").attr("src", imageSrc);
     $("#overlay").fadeIn();
