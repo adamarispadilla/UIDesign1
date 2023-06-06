d1b077866901f8e768faca7d7e58ca94865f7583
adamarispadilla <72954890+adamarispadilla@users.noreply.github.com>
2023-06-06 10:12:46 +0900

    js

--- P2/assets/css/script.js
+++ P2/assets/css/script.js
@@ -1,16 +1,25 @@
+const images = [...document.querySelectorAll('.image')];
 
-<script>
-$(document).ready(function() {
-  $('.overlay-trigger').click(function(e) {
-    e.preventDefault();
-    var imageUrl = $(this).find('img').attr('src');
-    $('#overlay-image').attr('src', imageUrl);
-    $('#overlay').fadeIn();
-  });
+// popup
 
-  $('#overlay').click(function() {
-    $(this).fadeOut();
-  });
-});
+const popup = document.querySelector('.popup');
+const largeImage = document.querySelector('.large-image');
+const imageIndex = document.querySelector('.index');
 
-</script>
+let index = 0; // will track our current image;
+
+images.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        updateImage(i);
+        popup.classList.toggle('active');
+    })
+})
+
+const updateImage = (i) => {
+    let path = `assets/css/relax/rcloud1${i+1}.png`;
+    largeImage.src = path;
+    imageName.innerHTML = path;
+    imageIndex.innerHTML = `0${i+1}`;
+    index = i;
+}
+
