a61fdb3dd639e4b0e4d549f84cd9bff3dffb0f0f
adamarispadilla <72954890+adamarispadilla@users.noreply.github.com>
2023-06-08 12:57:31 +0900

    Update script.js

--- P2/assets/js/script.js
+++ P2/assets/js/script.js
@@ -19,6 +19,6 @@ $(document).ready(function() {
   });
   var button = document.getElementById("button-external");
   button.addEventListener("click", function() {
-  window.location.href = "https://www.pixelthoughts.co/#";
+  window.location.href = "https://adamarispadilla.github.io/UIDesign1/";
   });
 });
